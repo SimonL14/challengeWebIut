6cea9d1f5c91897f96732df319ccc480e7f915c9
Telath <clement.pouillart@gmail.com>
2023-03-06 14:34:36 +0100

    Ajout de la gestion des utilisateurs

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
