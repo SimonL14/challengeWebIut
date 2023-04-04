1f6748e26f0bfba2a7c90f2ec0cf5307af8769f3
Marie-Camille Petit <76949730+MarieCamillePetit@users.noreply.github.com>
2023-04-04 18:03:12 +0200

    Modification style création d'événement
    - Modification du style de la page de création d'événement

--- src/Form/EventType.php
+++ src/Form/EventType.php
@@ -6,20 +6,36 @@ use App\Entity\Event;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 
 class EventType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('Name')
+            ->add('Name', null, [
+                'label' => 'Nom de l\'événement',
+            ])
             ->add('Creator')
-            ->add('Date_debut')
-            ->add('Date_fin')
-            ->add('description')
-            ->add('lieu')
-            ->add('image')
-            ->add('Id_depart');
+            ->add('Date_debut', null, [
+                'label' => 'Date de début',
+            ])
+            ->add('Date_fin', null, [
+                'label' => 'Date de fin',
+            ])
+            ->add('description', null, [
+                'label' => 'Description  ',
+            ])
+            ->add('lieu', null, [
+                'label' => 'Lieu',
+            ])
+            ->add('image', FileType::class, [
+                'required' => true,
+                'label' => 'Sélection image'
+            ])
+            ->add('Id_depart', null, [
+                'label' => 'Département',
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
